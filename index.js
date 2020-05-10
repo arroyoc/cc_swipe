17b145f3b7f38533fa484b85af947ebdf46fe834
Chris Arroyo <arroyoc91@gmail.com>
2020-05-10 15:48:26 -0600

    s

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var stepper;
 let ready = false;
 
 const validSwipeDistance = 95
-const validFowardSwipeSpeed = 60
+const validFowardSwipeSpeed = 75
 const validReturnSwipeSpeed = 25
 
 const invalidSwipeDistance = 50
