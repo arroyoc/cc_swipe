dca230f3310ca6080800bad03c9e341045cb669a
Chris Arroyo <arroyoc91@gmail.com>
2020-07-20 13:41:37 -0600

    rotations

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ var board = new five.Board();
 var stepper;
 let ready = false;
 
-const validSwipeDistance = 200
-const validFowardSwipeSpeed = 55
-const validReturnSwipeSpeed = 25
+const fullRotationDistance = 400
+const halfRotationDistance = 200
+const validSwipeSpeed = 55
+const invalidSwipeSpeed = 25
 
 const invalidSwipeDistance = 50
-const invalidFowardSwipeSpeed = 25
-const invalidReturnSwipeSpeed = 25
+
 
 board.on("ready", function() {
     var k = 0; 
@@ -28,36 +28,47 @@ board.on("ready", function() {
 
         
 //The following request will push the credit card through the reader performing a successful swipe
-app.get('/validswipe', (req, res) => {
+// app.get('/validswipe', (req, res) => {
+//     if (ready) {
+//         stepper.rpm(validFowardSwipeSpeed).cw().step(validSwipeDistance, function() { 
+// 	    setTimeout(() => {
+//                 stepper.rpm(validReturnSwipeSpeed).ccw().step(validSwipeDistance, function() { 
+// 		    console.log("Done stepping!"); 
+// 	        }); 
+//              }, 500);
+//             res.send('Valid swipe initiated..')
+//         });
+//     } else {
+//         res.send('Board not ready!');
+//     }
+// });
+
+//The following request will perform a full 360 degree rotation
+app.get('/fullrotation', (req, res) => {
     if (ready) {
-        stepper.rpm(validFowardSwipeSpeed).cw().step(validSwipeDistance, function() { 
-	    setTimeout(() => {
-                stepper.rpm(validReturnSwipeSpeed).ccw().step(validSwipeDistance, function() { 
-		    console.log("Done stepping!"); 
-	        }); 
-             }, 500);
-            res.send('Valid swipe initiated..')
+        stepper.rpm(validSwipeSpeed).ccw().step(fullRotationDistance, function() { 
+            res.send('full rotation initiated..')
         });
     } else {
         res.send('Board not ready!');
     }
 });
 
-//The following request will push the credit card through the reader performing a successful swipe
-app.get('/fullswipe', (req, res) => {
+//The following request will perofrm a half 180 degree rotation
+app.get('/halfrotation', (req, res) => {
     if (ready) {
-        stepper.rpm(validFowardSwipeSpeed).ccw().step(validSwipeDistance, function() { 
-            res.send('full swipe initiated..')
+        stepper.rpm(validSwipeSpeed).ccw().step(halfRotationDistance, function() { 
+            res.send('half rotation initiated..')
         });
     } else {
         res.send('Board not ready!');
     }
 });
 
-//The following request will push the credit card through the reader very slowly. resulting in an invalid swipe error
-app.get('/slowswipe', (req, res) => {
+//The following request will slowly rotate resulting in an invalid swipe
+app.get('/slowrotation', (req, res) => {
     if (ready) {
-        stepper.rpm(invalidFowardSwipeSpeed).cw().step(validSwipeDistance, function() { 
+        stepper.rpm(invalidSwipeSpeed).cw().step(fullRotationDistance, function() { 
             res.send('slow swipe initiated..')
         });
     } else {
@@ -66,37 +77,37 @@ app.get('/slowswipe', (req, res) => {
 });
 
 //The following request will push the credit card through the reader performing a successful swipe
-app.get('/testswipe', (req, res) => {
-    if (ready) {
-        stepper.step({ steps: 80, direction: 1, accel: 1000, decel: 100 }, function(){
-        // stepper.rpm(validFowardSwipeSpeed).cw().step(validSwipeDistance, function() { 
-	    setTimeout(() => {
-            stepper.step({ steps: 80, direction: 0, accel: 500, decel: 100 }, function() { 
-		    console.log("Done stepping!"); 
-	        }); 
-             }, 500);
-            res.send('Valid swipe initiated..')
-        });
-    } else {
-        res.send('Board not ready!');
-    }
-});
+// app.get('/testswipe', (req, res) => {
+//     if (ready) {
+//         stepper.step({ steps: 80, direction: 1, accel: 1000, decel: 100 }, function(){
+//         // stepper.rpm(validFowardSwipeSpeed).cw().step(validSwipeDistance, function() { 
+// 	    setTimeout(() => {
+//             stepper.step({ steps: 80, direction: 0, accel: 500, decel: 100 }, function() { 
+// 		    console.log("Done stepping!"); 
+// 	        }); 
+//              }, 500);
+//             res.send('Valid swipe initiated..')
+//         });
+//     } else {
+//         res.send('Board not ready!');
+//     }
+// });
 
 //The following request will only push the credit card half way through the reader performing an invalid swipe
-app.get('/invalidswipe', (req, res) => {
-    if (ready) {
-        stepper.rpm(invalidFowardSwipeSpeed).cw().step(invalidSwipeDistance, function() { 
-	    setTimeout(() => {
-                stepper.rpm(invalidReturnSwipeSpeed).ccw().step(invalidSwipeDistance, function() { 
-		    console.log("Done stepping!"); 
-	        }); 
-             }, 500);
-            res.send('Invalid swipe initiated..')
-        });
-    } else {
-        res.send('Board not ready!');
-    }
-});
+// app.get('/invalidswipe', (req, res) => {
+//     if (ready) {
+//         stepper.rpm(invalidFowardSwipeSpeed).cw().step(invalidSwipeDistance, function() { 
+// 	    setTimeout(() => {
+//                 stepper.rpm(invalidReturnSwipeSpeed).ccw().step(invalidSwipeDistance, function() { 
+// 		    console.log("Done stepping!"); 
+// 	        }); 
+//              }, 500);
+//             res.send('Invalid swipe initiated..')
+//         });
+//     } else {
+//         res.send('Board not ready!');
+//     }
+// });
 
 
 //Start of Small Bot
